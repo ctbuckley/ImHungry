e564cbfc090a420a70e3be5e2f08a10d3e19d194
Emily Jin <jin914@usc.edu>
2019-03-31 20:19:56 -0700

    search history servlet tests

---
+++ src/test/java/servlettests/AddSearchHistoryServletTest.java
@@ -0,0 +1,91 @@
+package servlettests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import data.Database;
+import data.SearchItem;
+import servlets.AddSearchHistoryServlet;
+
+/*
+ *  Tests for the ResultsPageServlet class.
+ */
+public class AddSearchHistoryServletTest {
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+    @Mock
+    HttpSession session;
+    @Mock
+    RequestDispatcher rd;
+    
+    Database db;
+    int userID;
+    
+    @Before
+    public void setUp() throws ClassNotFoundException, SQLException {
+        MockitoAnnotations.initMocks(this);
+        
+        db = new Database();
+		db.insertUserintoUsers("testUser", "password");
+		ResultSet rs =  db.getUserfromUsers("testUser");
+		rs.next();
+		userID = rs.getInt("userID");
+		
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        session = mock(HttpSession.class);
+        rd = mock(RequestDispatcher.class);
+        
+        when(request.getSession()).thenReturn(session);
+        
+    }
+    /*
+     *  Test to make sure a valid new user returns correctly.
+     */
+    @Test
+    public void testValidNewUser() throws Exception {
+       when(request.getParameter("username")).thenReturn("testUser");
+       when(request.getParameter("radius")).thenReturn("10000");
+       when(request.getParameter("searchQuery")).thenReturn("milk tea");
+       when(request.getParameter("numResults")).thenReturn("7");
+        
+       new AddSearchHistoryServlet.service(request, response);
+ 
+       ArrayList<SearchItem> si = db.getSearchItemfromSearch(userID);
+       int searchID = si.get(0).searchID;
+       
+       assertEquals(si.get(0).searchQuery, "milk tea");
+       assertEquals(si.get(0).numResults, 7); 
+       assertEquals(si.get(0).radius, 10000);
+       
+       db.deleteQueryfromSearchHistory(searchID);
+       
+       
+    } 
+    
+    @After
+	public void teardown() throws SQLException {
+		db.deleteUserfromUsers(userID);
+	}
+
+
+    
+}
