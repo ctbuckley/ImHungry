97e6b074a5edec77c1ac3bcfd60062cb2d1d76c6
AlexColello <acolello@usc.edu>
2019-04-06 01:46:37 -0700

    More travis changes

--- deploy_application.sh
+++ deploy_application.sh
@@ -1,9 +1,9 @@
-ls /usr/share
+ls /opt
 ls /home/travis/build/AlexColello/CS310GroupC/target
 ls /home/travis/build/AlexColello/CS310GroupC/target/FeedMe
 # My tomcat webapps are found at /var/lib/tomcat6/webapps
 # The application I wish to deploy is the main (ROOT) application
-webapps_dir=/usr/share/tomcat9/webapps
+webapps_dir=/opt/tomcat/webapps
 # Remove existing assets (if any)
 rm -rf $webapps_dir/FeedMe
 # Copy WAR file into place
