a83a6f9c2ba0b573380e32255fe0cc12f26211a9
AlexColello <acolello@usc.edu>
2019-04-06 02:00:07 -0700

    Hopefully fixed an issue

--- deploy_application.sh
+++ deploy_application.sh
@@ -1,14 +1,13 @@
 ls /opt
 ls /home/travis/build/AlexColello/CS310GroupC/target
-ls /home/travis/build/AlexColello/CS310GroupC/target/FeedMe
 # My tomcat webapps are found at /var/lib/tomcat6/webapps
 # The application I wish to deploy is the main (ROOT) application
 webapps_dir=/opt/tomcat/webapps
 # Remove existing assets (if any)
 rm -rf $webapps_dir/FeedMe
 
-jar -cvf FeedMe.war /home/travis/build/AlexColello/CS310GroupC/target/FeedMe/*  
-
+jar -cvf /home/travis/build/AlexColello/CS310GroupC/target/FeedMe.war /home/travis/build/AlexColello/CS310GroupC/target/FeedMe/*  
+ls /home/travis/build/AlexColello/CS310GroupC/target
 # Copy WAR file into place
 cp /home/travis/build/AlexColello/CS310GroupC/target/FeedMe.war $webapps_dir
 # Restart tomcat
