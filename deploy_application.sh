4459a7eb125d5a07b37529493cda116081284abf
Alex Colello <acolello@usc.edu>
2019-04-05 23:17:30 -0700

    Update deploy_application.sh

--- deploy_application.sh
+++ deploy_application.sh
@@ -1,1 +1,10 @@
-cat /home/travis/.m2/settings.xml
+ls /var/lib/
+# My tomcat webapps are found at /var/lib/tomcat6/webapps
+# The application I wish to deploy is the main (ROOT) application
+webapps_dir=/var/lib/tomcat9/webapps
+# Remove existing assets (if any)
+rm -rf $webapps_dir/ROOT
+# Copy WAR file into place
+cp /home/travis/build/AlexColello/CS310GroupC/target/ROOT.war $webapps_dir
+# Restart tomcat
+service tomcat restart
