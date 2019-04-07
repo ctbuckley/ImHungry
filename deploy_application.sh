7de0171448ccde637995c0455c8ce4328e316e1c
AlexColello <acolello@usc.edu>
2019-04-06 21:41:47 -0700

    Trying with no sleep

--- deploy_application.sh
+++ deploy_application.sh
@@ -11,7 +11,5 @@ cp /home/travis/build/AlexColello/CS310GroupC/target/FeedMe.war $webapps_dir
 # Restart tomcat
 sudo systemctl restart tomcat
 
-sleep 45
-
 sudo systemctl status tomcat
 curl -v -u main_user:main_password http://127.0.0.1:8080/manager/text/list
