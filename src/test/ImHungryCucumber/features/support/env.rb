dba38d8c2e25904e939f6127b3b71ab1548264e3
AlexColello <acolello@usc.edu>
2019-04-06 18:34:18 -0700

    Updated env.rb

--- src/test/ImHungryCucumber/features/support/env.rb
+++ src/test/ImHungryCucumber/features/support/env.rb
@@ -8,7 +8,7 @@ Capybara.register_driver :chrome do |app|
 	capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
 	  chromeOptions: { args: %w(headless disable-gpu) }
 	)
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>'./features/support/chromedriver', desired_capabilities: => capabilities)
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>'./features/support/chromedriver', :desired_capabilities => capabilities)
 end
 
 Capybara.default_driver = :chrome
