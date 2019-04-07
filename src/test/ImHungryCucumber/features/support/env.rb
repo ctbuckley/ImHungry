e52eabcb1ebfa693e366afce56b0a0d00cad2609
AlexColello <acolello@usc.edu>
2019-04-06 18:28:50 -0700

    trying to do headless for selenium

--- src/test/ImHungryCucumber/features/support/env.rb
+++ src/test/ImHungryCucumber/features/support/env.rb
@@ -5,7 +5,10 @@ require 'rspec'
 
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>'./features/support/chromedriver')
+	capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+	  chromeOptions: { args: %w(headless disable-gpu) }
+	)
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>'./features/support/chromedriver', desired_capabilities: => capabilities)
 end
 
 Capybara.default_driver = :chrome
