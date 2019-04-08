a0dd4981e0cabd00deb182890a851e4a340c0256
Will Borie <borie@usc.edu>
2019-04-07 21:46:46 -0700

    Fixed a mistake

--- src/test/ImHungryCucumber/features/support/env.rb
+++ src/test/ImHungryCucumber/features/support/env.rb
@@ -5,7 +5,11 @@ require 'rspec'
 require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/usr/local/bin/chromedriver")
+	options = Selenium::WebDriver::Chrome::Options.new
+	options.add_argument('--headless')
+	options.add_argument('--disable-gpu')
+	options.add_argument('--window-size=750,750')
+    Capybara::Selenium::Driver.new(app, browser: :chrome, driver_path: './features/support/chromedriver', options: options)
 end
 
 Capybara.default_driver = :chrome
