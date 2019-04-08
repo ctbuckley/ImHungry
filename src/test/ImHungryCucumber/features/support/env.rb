2aee65dbc3605de46aa95e38ece176d5761a7b0d
AlexColello <acolello@usc.edu>
2019-04-07 18:06:37 -0700

    Added tests in List management

--- src/test/ImHungryCucumber/features/support/env.rb
+++ src/test/ImHungryCucumber/features/support/env.rb
@@ -8,7 +8,8 @@ Capybara.register_driver :chrome do |app|
 	options = Selenium::WebDriver::Chrome::Options.new
 	options.add_argument('--headless')
 	options.add_argument('--disable-gpu')
-    Capybara::Selenium::Driver.new(app, browser: :chrome, driver_path: './features/support/chromedriver', options: options)
+	options.add_argument('--window-size=750,750')
+    Capybara::Selenium::Driver.new(app, browser: :chrome, driver_path: './features/support/chromedriver.exe', options: options)
 end
 
 Capybara.default_driver = :chrome
