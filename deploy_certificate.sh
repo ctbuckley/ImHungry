38d2eb0e4130eb1e48fc3c16fd58002645b8a758
AlexColello <acolello@usc.edu>
2019-04-09 22:49:25 -0700

    test

--- deploy_certificate.sh
+++ deploy_certificate.sh
@@ -1,5 +1,7 @@
 sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/server.xml /opt/tomcat/conf/server.xml
-ls /opt/tomcat/conf
+sudo ls /opt/tomcat/conf
 sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/rootCA.pem /etc/ssl/certs/rootCA.pem
-ls /etc/ssl/certs
+sudo ls /etc/ssl/certs
+ls /opt/tomcat
+ls /home/travis/build/AlexColello/CS310GroupC/configuration_files
 sudo update-ca-certificates
