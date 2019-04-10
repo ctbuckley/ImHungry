3a8db4040846cd3009bcf6905c6474e678adb411
AlexColello <acolello@usc.edu>
2019-04-09 22:40:45 -0700

    Trying to make travis work

--- deploy_certificate.sh
+++ deploy_certificate.sh
@@ -1,4 +1,5 @@
-cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/server.xml /opt/tomcat/conf/server.xml
-
+sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/server.xml /opt/tomcat/conf/server.xml
+ls /opt/tomcat/conf/
 sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/rootCA.pem /usr/local/share/ca-certificates/rootCA.pem
+ls /usr/local/share/ca-certificates/
 sudo update-ca-certificates
