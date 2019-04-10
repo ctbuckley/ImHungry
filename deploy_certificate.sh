bd8c2dd21923d6855f61870a249b2d0b40ee94ee
AlexColello <acolello@usc.edu>
2019-04-09 22:54:24 -0700

    Travis fix?

--- deploy_certificate.sh
+++
@@ -1,7 +0,0 @@
-sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/server.xml /opt/tomcat/conf/server.xml
-sudo ls /opt/tomcat/conf
-sudo cp /home/travis/build/AlexColello/CS310GroupC/configuration_files/rootCA.pem /etc/ssl/certs/rootCA.pem
-sudo ls /etc/ssl/certs
-ls /opt/tomcat
-ls /home/travis/build/AlexColello/CS310GroupC/configuration_files
-sudo update-ca-certificates
