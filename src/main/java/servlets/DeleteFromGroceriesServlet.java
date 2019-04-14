5eba10ceb91a73188b70c5d92a925bec880f7825
AlexColello <acolello@usc.edu>
2019-04-13 20:14:19 -0700

    Added new servlet and fix to diplay groceries servlet.

--- src/main/java/servlets/DeleteFromGroceriesServlet.java
+++ src/main/java/servlets/DeleteFromGroceriesServlet.java
@@ -1,4 +1,3 @@
-
 package servlets;
 
 import java.io.IOException;
@@ -7,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,17 +13,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import data.Database;
 
-@WebServlet("/DisplayGroceryList")
-public class DisplayGroceryListServlet extends HttpServlet {
+@WebServlet("/DeleteFromGroceries")
+public class DeleteFromGroceriesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String username = (String)session.getAttribute("username");
 		
-		String[] groceryList = new String[0];
-
+		String item = request.getParameter("item");
 		try {
 			Database db = new Database();
 			ResultSet rs = db.getUserfromUsers(username);
@@ -33,19 +34,15 @@ public class DisplayGroceryListServlet extends HttpServlet {
 			int userID = rs.getInt("userID");
 			rs.close();
 			ArrayList<String> currentGroceryItems = db.getGroceryListforUser(userID);
+			if (currentGroceryItems.contains(item)) {
+				db.deleteIngredientfromGrocery(userID, item);
+			}
 			
-			groceryList = new String[currentGroceryItems.size()];
-			currentGroceryItems.toArray(groceryList);
-	
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		session.setAttribute("groceryList", groceryList);
-		
-		RequestDispatcher dispatch = request.getRequestDispatcher("/jsp/groceryList.jsp");
-		dispatch.forward(request,  response);	
 	}
+
 }
